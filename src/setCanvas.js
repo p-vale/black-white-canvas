6a07a97d5b623d0cfdf595a08b296986567ce0a2
Valentina Pezzi <valente.pezzi@gmail.com>
2022-04-17 09:54:35 +0200

    mobile responsive

--- src/setCanvas.js
+++ src/setCanvas.js
@@ -1,8 +1,16 @@
 function setCanvas() {
     let canvas = document.createElement("canvas")
     let context = canvas.getContext("2d")
-    const canvasW = 500
-    const canvasH = 500
+    let canvasW = 500
+    let canvasH = 500
+    let screenWidth = window.screen.width
+    if (screenWidth < 600) {
+        canvasW = 400
+        canvasH = 400
+    } else if (screenWidth < 400) {
+        canvasW = 300
+        canvasH = 300
+    }
     canvas.width = canvasW
     canvas.height = canvasH
 
